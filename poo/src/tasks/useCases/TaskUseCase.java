84253d0983723e370587883337e333e0c3875659
Estefania-Catano <ecatano1@cesde.net>
2025-08-01 21:00:45 -0500

    semana4

---
+++ poo/src/tasks/useCases/TaskUseCase.java
@@ -0,0 +1,48 @@
+package tasks.useCases;
+
+import tasks.models.Task;
+
+// Inconsistencias en un hilo
+
+// Estado de un hilo
+
+// Contenerdor de metodos y atributos de la tarea
+public class TaskUseCase {
+    // propiedades
+    List<Task> tasks; // lista vacía de tareas
+
+    // Constructor
+    public TaskUseCase() {
+    }
+
+    // Lista de tareas y queremos agregar una tarea a la lista de tareas
+    public String create(String name, String description) {
+
+        Task task = new Task(name, description, false);
+        this.tasks.add(task);
+        return "Tarea Creada: " + task.getName() + " Descripcion: " + task.getDescription() + " " + task.getStatus();
+    }
+}
+
+// Uptade
+
+public String update(Task task;String name; String description;boolean satus){
+
+task.setName(name);
+task.setDescription(description);
+task.setStatus(status);
+        return "Tarea Actualizada: " + task.getName() + " Descripcion: " + task.getDescription() + " Estado: " + task.getStatus();
+    }
+
+
+// Delete
+public String delete(Task task) {
+    task = null;
+    return "Tarea Eliminada";
+}
+
+
+// Get
+public String get() {
+    return "Tarea: " + task.getName() + " Descripcion: " + task.getDescription() + " Estado: " + task.getStatus();
+}
