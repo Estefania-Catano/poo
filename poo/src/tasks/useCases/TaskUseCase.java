eea853d14750d2f2f3c08dc04bd76367c6295d6f
Lizeth Marin <lizeth.marin6485@gmail.com>
2025-08-29 19:24:19 -0500

    Inicio clase 29/08/2025

--- poo/src/tasks/useCases/TaskUseCase.java
+++ poo/src/tasks/useCases/TaskUseCase.java
@@ -19,35 +19,41 @@ public class TaskUseCase {
         this.tasks = new ArrayList<>();
     }
 
-
     // Lista de tareas y queremos agregar una tarea a la lista de tareas
     public String create(String name, String description) {
+        try {
+            Task task = new Task(name, description, false);
+            this.tasks.add(task);
+            return "Tarea Creada: " + task.getName() + " Descripcion: " + task.getDescription();
+        } catch (Exception e) {
+            return "Ha ocurrido un error, por favor intente de nuevo";
+        }
+    }
 
-        Task task = new Task(name, description, false);
-        this.tasks.add(task);
-        return "Tarea Creada: " + task.getName() + " Descripcion: " + task.getDescription() + " " + task.getStatus();
+    // Uptade
+
+public String update(int index;String name; String description;){
+try{
+    Task taskFound = new  Task();
+for (int i=0; i< task.size();i++){
+    if(index == i){
+        Task task = tasks.get(index);
+        task.setName(name);
+        task.setDescription(description);
+        taskFound = task;
+    }
     }
 }
 
-// Uptade
+ }
 
-public String update(Task task;String name; String description;boolean satus){
-
-task.setName(name);
-task.setDescription(description);
-task.setStatus(status);
-        return "Tarea Actualizada: " + task.getName() + " Descripcion: " + task.getDescription() + " Estado: " + task.getStatus();
+    // Delete
+    public String delete(Task task) {
+        task = null;
+        return "Tarea Eliminada";
     }
 
-
-// Delete
-public String delete(Task task) {
-    task = null;
-    return "Tarea Eliminada";
-}
-
-
-// Get
+    // Get
 public String get() {
     return "Tarea: " + task.getName() + " Descripcion: " + task.getDescription() + " Estado: " + task.getStatus();
 }
